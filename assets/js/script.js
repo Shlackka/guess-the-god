531a8149f3009beaf50c1cc69946f6c05ffed288
Oliver Morse <Olimorse_90@Hotmail.co.uk>
2024-06-11 14:29:45 +0000

    Minor JS changes to ensure formatting correct in game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,52 @@
 
 const gods = [
     { image: 'assets/images/gods/zeus.webp', alt: 'this is a picture of a greek god', mythology: 'Greek', hint: 'King of the gods and ruler of Mount Olympus',
-        name: 'Zeus', power: 'god of the sky, lightning, and thunder',
+        name: 'Zeus', power: 'god of the sky, lightning, and thunder.',
     },
     { image: 'assets/images/gods/poseidon.webp', alt: 'this is a picture of a greek god', mythology: 'Greek', hint: 'God of the sea, often depicted with a trident',
-        name: 'Poseidon', power: 'god of the sea, earthquakes, and storms',
+        name: 'Poseidon', power: 'god of the sea, earthquakes, and storms.',
     },
     { image: 'assets/images/gods/athena.webp', alt: 'this is a picture of a greek god', mythology: 'Greek', hint: 'Goddess of wisdom and war, often associated with an owl',
-        name: 'Athena', power: 'goddess of wisdom, courage, and war',
+        name: 'Athena', power: 'goddess of wisdom, courage, and war.',
     },
     { image: 'assets/images/gods/hades.webp', alt: 'this is a picture of a greek god', mythology: 'Greek', hint: 'Ruler of the underworld and the dead',
-        name: 'Hades', power: 'god of the underworld and the dead',
+        name: 'Hades', power: 'god of the underworld and the dead.',
     },
     { image: 'assets/images/gods/ra.webp', alt: 'this is a picture of a egyptian god', mythology: 'Egyptian', hint: 'Sun god and ruler of the sky, often depicted with a sun disk',
-        name: 'Ra', power: 'god of the sun and order',
+        name: 'Ra', power: 'god of the sun and order.',
     },
     { image: 'assets/images/gods/osiris.webp', alt: 'this is a picture of a egyptian god', mythology: 'Egyptian', hint: 'God of the afterlife and resurrection, often depicted in mummy wrappings',
-        name: 'Osiris', power: 'god of the afterlife and resurrection',
+        name: 'Osiris', power: 'god of the afterlife and resurrection.',
     },
     { image: 'assets/images/gods/anubis.webp', alt: 'this is a picture of a egyptian god', mythology: 'Egyptian', hint: 'God of mummification and the afterlife, often shown with a jackal head',
-        name: 'Anubis', power: 'god of mummification and the afterlife',
+        name: 'Anubis', power: 'god of mummification and the afterlife.',
     },
     { image: 'assets/images/gods/horus.webp', alt: 'this is a picture of a egyptian god', mythology: 'Egyptian', hint: 'Sky god, often depicted with a falcon head',
-        name: 'Horus', power: 'god of the sky and kingship',
+        name: 'Horus', power: 'god of the sky and kingship.',
     },
     { image: 'assets/images/gods/odin.webp', alt: 'this is a picture of a norse god', mythology: 'Norse', hint: 'Allfather and god of wisdom, often depicted with one eye and ravens',
-        name: 'Odin', power: 'god of wisdom, poetry, and death',
+        name: 'Odin', power: 'god of wisdom, poetry, and death.',
     },
     { image: 'assets/images/gods/thor.webp', alt: 'this is a picture of a norse god', mythology: 'Norse', hint: 'God of thunder, lightning, and storms, known for his hammer Mjolnir',
-        name: 'Thor', power: 'god of thunder, lightning, and storms',
+        name: 'Thor', power: 'god of thunder, lightning, and storms.',
     },
     { image: 'assets/images/gods/loki.webp', alt: 'this is a picture of a norse god', mythology: 'Norse', hint: 'Trickster god known for his mischief and shapeshifting abilities',
-        name: 'Loki', power: 'god of mischief and trickery',
+        name: 'Loki', power: 'god of mischief and trickery.',
     },
     { image: 'assets/images/gods/freyja.webp', alt: 'this is a picture of a norse god', mythology: 'Norse', hint: 'Goddess of love, beauty, and fertility, often associated with cats and a chariot',
-        name: 'Freyja', power: 'goddess of love, beauty, and fertility',
+        name: 'Freyja', power: 'goddess of love, beauty, and fertility.',
     },
     { image: 'assets/images/gods/huitzilopochtli.webp', alt: 'this is a picture of a aztec god', mythology: 'Aztec', hint: 'God of war and the sun, patron of Tenochtitlan',
-        name: 'Huitzilopochtli', power: 'god of war and the sun'
+        name: 'Huitzilopochtli', power: 'god of war and the sun.',
     },
     { image: 'assets/images/gods/quetzalcoatl.webp', alt: 'this is a picture of a aztec god', mythology: 'Aztec', hint: 'Feathered serpent god of wind and learning',
-        name: 'Quetzalcoatl', power: 'god of wind and learning'
+        name: 'Quetzalcoatl', power: 'god of wind and learning.',
     },
     { image: 'assets/images/gods/tezcatlipoca.webp', alt: 'this is a picture of a aztec god', mythology: 'Aztec', hint: 'God of the night sky and ancestral memory',
-        name: 'Tezcatlipoca', power: 'god of the night sky and ancestral memory'
+        name: 'Tezcatlipoca', power: 'god of the night sky and ancestral memory.',
     },
     { image: 'assets/images/gods/tlaloc.webp', alt: 'this is a picture of a aztec god', mythology: 'Aztec', hint: 'God of rain, water, and fertility',
-        name: 'Tlaloc', power: 'god of rain, water, and fertility'
+        name: 'Tlaloc', power: 'god of rain, water, and fertility.',
     }
 
 ];
@@ -134,10 +134,11 @@ function incrementScore() {
 function checkAnswer (selectedMythology) {
     let god = gods[currentGod];
     if (selectedMythology === god.mythology) {
-        showMessage(`${god.mythology} is correct! That was ${god.name}, ${god.power}`);
+        showMessage(`${god.mythology} is correct!<br><br>That was ${god.name}, ${god.power}`);
         incrementScore();
     } else {
-        showMessage(`${selectedMythology} is incorrect the correct answer is ${god.mythology}.`);
+        showMessage(`${selectedMythology} is incorrect the correct answer is ${god.mythology}.
+            <br><br> That was ${god.name}, ${god.power}`);
     }
     startGame();
 }
@@ -221,7 +222,7 @@ function showMessage(message) {
     let alertBox = document.getElementById("alert-box");
     let alertMessage = document.getElementById("custom-message")
 
-    alertMessage.textContent = message;
+    alertMessage.innerHTML = message;
     alertBox.classList.remove("hidden");
 }
 
